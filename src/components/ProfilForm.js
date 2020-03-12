dc9339f7c72284886b9adb65e6492a7ac03dd9cd
stefaniordache <stefan.iordache2001@gmail.com>
2020-03-12 19:31:29 +0200

    picker judete&localitati

--- src/components/ProfilForm.js
+++ src/components/ProfilForm.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Picker, StyleSheet, View} from "react-native";
 import {ButtonGroup, Input} from "react-native-elements";
 import Spacer from "./Spacer";
@@ -9,20 +9,65 @@ const ProfilForm = () => {
 
     const [gen, setGen] = useState(0);
     const [dataNastere, setDataNastere] = useState(null);
+    const [judet, setJudet] = useState(0);
+    const [localitate, setLocalitate] = useState(0);
+    const [localitati, setLocalitati] = useState([{id:1, nume: "Calarasi", judet: 1}, {id:2, nume: "Modelu", judet: 1},
+        {id:3, nume: "Bucale", judet: 2}, {id:4, nume: "Constanta", judet: 2}]);
 
     const {data} = useContext(AppContext);
 
-    const updateGender = (selectedIndex) => {
-        setGen(selectedIndex);
+    useEffect(() => {
+
+    }, [data]);
+
+    const renderJudete = () => {
+          return data.judete.map((item) => {
+              return <Picker.Item label={item.nume} value={item.id} key={item.id}/>;
+          });
     };
 
-    const {selectedIndex} = gen;
+    const renderLocalitati = () => {
+        return localitati.filter((item) => item.judet === judet)
+            .map((item) => {
+            return <Picker.Item label={item.nume} value={item.id} key={item.id}/>;
+        });
+    };
 
     return (
         <View style={styles.container}>
             <Input label={"Nume"} />
             <Input label={"Prenume"} />
             <Input label={"Telefon"} keyboardType={"numeric"} />
+            <Spacer />
+            <View style={styles.dropDown}>
+                <Picker
+                    style={styles.picker}
+                    itemStyle={styles.pickerItem}
+                    selectedValue={judet}
+                    onValueChange={(itemValue) => {
+                        setJudet(itemValue);
+                        console.log(itemValue);
+                    }}
+                >
+                    <Picker.Item label="--Selectati judet--" value={0} key={0} />
+                    {renderJudete()}
+                </Picker>
+            </View>
+            <Spacer />
+            <View style={styles.dropDown}>
+                <Picker
+                    style={styles.picker}
+                    itemStyle={styles.pickerItem}
+                    selectedValue={localitate}
+                    onValueChange={(itemValue) => {
+                        setLocalitate(itemValue);
+                        console.log(itemValue);
+                    }}
+                >
+                    <Picker.Item label="--Selectati localitatea--" value={0} key={0} />
+                    {renderLocalitati()}
+                </Picker>
+            </View>
             <Spacer />
             <DatePicker
                 placeholder={"Data nasterii"}
@@ -33,7 +78,7 @@ const ProfilForm = () => {
                 onDateChange={date => setDataNastere(date)}
                 date={dataNastere}
             />
-            <ButtonGroup buttons={['Barbat', 'Femeie']} onPress={updateGender} selectedIndex={selectedIndex} />
+            <ButtonGroup buttons={['Barbat', 'Femeie']} onPress={setGen} selectedIndex={gen} />
         </View>
     );
 };
@@ -48,7 +93,22 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         padding: 10,
-    }
+    },
+    picker: {
+        height: Platform.OS === 'android' ? 50 : 132,
+    },
+    pickerItem: {
+        height: Platform.OS === 'android' ? 50 : 132,
+    },
+    dropDown: {
+        justifyContent: 'center',
+        // borderWidth: 1,
+        // borderColor: '#2796D6',
+        height: Platform.OS === 'android' ? 50 : 132,
+        borderRadius: 5,
+        marginBottom: 5,
+        width: '100%'
+    },
 });
 
 export default ProfilForm;
